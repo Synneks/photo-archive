a9ba957e43eccf24d5722f0426088f336e768c86
Denis Eneotescu <deniseneotescu@gmail.com>
2024-06-22 21:08:00 +0300

    modal improvements

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,13 +21,11 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="m-0 h-screen w-screen bg-zinc-900/70"
+      className="m-0 h-screen w-screen bg-zinc-900/70 text-white"
       onClose={onDismiss}
     >
       {children}
-      <button onClick={onDismiss} className="close-button">
-        Close
-      </button>
+      {/* <button onClick={onDismiss}>Close</button> */}
     </dialog>,
     document.getElementById("modal-root")!,
   );
--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -3,5 +3,19 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
 
-  return <img src={image.url} alt={image.name} className="h-96 w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0 bg-green-500">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="flex-shrink object-contain"
+        />
+      </div>
+      <div className="flex w-48 flex-col items-center justify-center border-l">
+        <h2 className="text-xl font-bold">{image.name}</h2>
+        <p className="text-lg">{image.createdAt.toISOString()}</p>
+      </div>
+    </div>
+  );
 }
